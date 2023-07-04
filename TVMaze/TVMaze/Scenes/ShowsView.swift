2835d79a5b8bc4de608a292a6d48dbfcb33fdcae
Gerson Arbigaus <gerson87@gmail.com>
2023-07-04 17:43:34 -0300

    Move the `Tag` and the `Card` struct to a `ShowView` extension file

--- TVMaze/TVMaze/Scenes/ShowsView.swift
+++ TVMaze/TVMaze/Scenes/ShowsView.swift
@@ -59,48 +59,6 @@ struct ShowsView: View {
         }
         .padding()
     }
-
-    private struct Tag: View {
-        let genre: String
-
-        var body: some View {
-            Text(genre)
-                .font(.caption)
-                .padding(4)
-                .background(
-                    RoundedRectangle(cornerRadius: 4)
-                        .foregroundColor(Color(genre))
-                )
-        }
-    }
-
-    private struct Card: View {
-        let show: TVShow
-
-        var body: some View {
-            HStack(spacing: 4) {
-                ImageView(imageUrl: show.image?.medium ?? "")
-                    .frame(height: 120)
-                    .cornerRadius(8)
-                VStack(alignment: .leading) {
-                    Text(show.name ?? "")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                    Text(show.language ?? "")
-                        .font(.subheadline)
-                    Spacer()
-                    HStack {
-                        ForEach(show.genres ?? [], id: \.self) { genre in
-                            Tag(genre: genre)
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(8)
-            }
-        }
-    }
 }
 
 struct ShowsView_Previews: PreviewProvider {
