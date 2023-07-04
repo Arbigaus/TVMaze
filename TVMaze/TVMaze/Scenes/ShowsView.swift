103e3d900fae4a08dbbceab3b7bf6fac04ac87f2
Gerson Arbigaus <gerson87@gmail.com>
2023-07-04 17:41:15 -0300

    Created a private struct called `Card` in the `ShowsView` to presente each Show

--- TVMaze/TVMaze/Scenes/ShowsView.swift
+++ TVMaze/TVMaze/Scenes/ShowsView.swift
@@ -28,27 +28,7 @@ struct ShowsView: View {
             List {
                 ForEach(viewModel.shows) { show in
                     NavigationLink(destination: DetailView()) {
-                        HStack(spacing: 4) {
-                            ImageView(imageUrl: show.image?.medium ?? "")
-                                .frame(height: 120)
-                                .cornerRadius(8)
-                            VStack(alignment: .leading) {
-                                Text(show.name ?? "")
-                                    .font(.headline)
-                                    .fontWeight(.bold)
-                                Text(show.language ?? "")
-                                    .font(.subheadline)
-                                Spacer()
-                                HStack {
-                                    ForEach(show.genres ?? [], id: \.self) { genre in
-                                        Tag(genre: genre)
-                                    }
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(8)
-                        }
+                        Card(show: show)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .task {
                             if self.viewModel.shows.isLast(show) {
@@ -91,6 +71,34 @@ struct ShowsView: View {
                     RoundedRectangle(cornerRadius: 4)
                         .foregroundColor(Color(genre))
                 )
+        }
+    }
+
+    private struct Card: View {
+        let show: TVShow
+
+        var body: some View {
+            HStack(spacing: 4) {
+                ImageView(imageUrl: show.image?.medium ?? "")
+                    .frame(height: 120)
+                    .cornerRadius(8)
+                VStack(alignment: .leading) {
+                    Text(show.name ?? "")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    Text(show.language ?? "")
+                        .font(.subheadline)
+                    Spacer()
+                    HStack {
+                        ForEach(show.genres ?? [], id: \.self) { genre in
+                            Tag(genre: genre)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(8)
+            }
         }
     }
 }
